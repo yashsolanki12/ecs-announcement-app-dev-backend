5a0643064c7f6050f64175f7ed03a59ec963e1e4
yashsolanki12 <yash@ecodesoft.com>
2026-05-27 13:22:27 +0530

    chore: Update the checkLength write condition in the same variable

--- dist/controllers/announcement.js
+++ dist/controllers/announcement.js
@@ -300,8 +300,8 @@ export const publicListAnnouncement = asyncHandler(async (req, res) => {
     }
     let response = await announcementService.getAllAnnouncement(filter);
     const increment = Math.floor(Math.random() * 8) + 1; // plan view number
-    const checkLength = response.map((i) => i).length;
-    if (checkLength > 0) {
+    const checkLength = response.map((i) => i).length > 0;
+    if (checkLength) {
         if (!metrics) {
             metrics = new StoreMetrics({
                 shop,
